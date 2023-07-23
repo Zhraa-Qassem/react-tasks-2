f62b9fc5898b075c526a8a967722f12677044cc4
zhraa <zhraa2005spn@gmail.com>
2023-07-23 05:16:40 +0300

    made counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function Child(){
-  console.log('this is child component');
-  return(
-  <h1 className='child' >hello this is zhraa calling from child component</h1>
+let count = 0;
 
-  )
+function Counter() {
+  const Click = () => {
+    count = count + 1;
+    document.querySelector('.counter').textContent = count;
+  };
 
-}
-function App() {
-  console.log('this is parent component');
   return (
-    <div className="App">
- <h1 className='parent'>this zhraa's tag grom parent container</h1>
- <Child/>
+    <div className="counter" onClick={Click}>
+      {count}
     </div>
     
   );
+}
 
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
+
+
